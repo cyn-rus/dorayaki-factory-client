62f192035821155dee4f6abf5a50fb1a518fbae4
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-15 20:47:26 +0700

    add compoents

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as Template } from './Template'
+export { default as RecipeCardContainer } from './RecipeCardContainer'
+export { default as MaterialCardContainer} from './MaterialCardContainer'
+export { default as AddRecipeModal } from './AddRecipeModal'
+export { default as AddMaterialModal } from './AddMaterialModal'
