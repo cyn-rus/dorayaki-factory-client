72a1b439cc30ae386e73918e7d211a017d6db7fd
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-15 20:48:21 +0700

    add pages

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Create } from './Create'
+export { default as Recipes } from './Recipes'
+export { default as Login } from './Login'
+export { default as Register } from './Register'
+export { default as Request } from './Request'
+export { default as Materials } from './Materials'
