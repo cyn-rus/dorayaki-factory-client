8f5a7e60437cd0e49d3b6193ece86cff8a55d885
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-15 20:47:13 +0700

    template component

---
+++ src/components/Template/index.tsx
@@ -0,0 +1,11 @@
+const Template = ({ children }: any) => {
+  return (
+    <div className='template'>
+      <nav className='navbar'>
+      </nav>
+      {children}
+    </div>
+  )
+}
+
+export default Template
