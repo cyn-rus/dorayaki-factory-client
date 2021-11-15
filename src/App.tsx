1715e219795380146f8bb8914ff4de1beb188f89
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-15 20:44:47 +0700

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { Create, Recipes, Materials, Login, Register, Request } from './pages'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route exact path='/'>
+        <Redirect to='/login' />
+      </Route>
+      <Switch>
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/recipes' component={Recipes} />
+        <Route exact path='/materials' component={Materials} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
