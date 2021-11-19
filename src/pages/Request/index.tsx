b7b7260a834dc8ecb86542fe1281c60248204553
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-19 12:43:46 +0700

    request page

--- src/pages/Request/index.tsx
+++ src/pages/Request/index.tsx
@@ -1,5 +1,13 @@
-const Request = () => {
+import { Template, RequestTable } from "../../components"
 
+const Request = () => {
+  return (
+    <Template>
+      <div className='request-page'>
+        <RequestTable />
+      </div>
+    </Template>
+  )
 }
 
 export default Request
