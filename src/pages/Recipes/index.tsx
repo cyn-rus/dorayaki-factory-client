d5eeb6e021389c78282bae7a698638911d16c7be
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-22 00:57:27 +0700

    add style for recipe page

--- src/pages/Recipes/index.tsx
+++ src/pages/Recipes/index.tsx
@@ -5,22 +5,37 @@ import { axios } from '../../api'
 
 const Recipes = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
-  const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState([{
+    nama_bahan: 'tes',
+    jumlah: 2,
+  }, {
+    nama_bahan: 'tes',
+    jumlah: 2,
+  }, {
+    nama_bahan: 'tes',
+    jumlah: 2,
+  }, {
+    nama_bahan: 'tes',
+    jumlah: 2,
+  }, {
+    nama_bahan: 'tes',
+    jumlah: 2,
+  }])
   const [materials, setMaterials] = useState([])
 
-  useEffect(() => {
-    axios.get('/materials')
-      .then(function (res) {
-        setMaterials(res.data)
-      })
-  }, [])
+  // useEffect(() => {
+  //   axios.get('/materials')
+  //     .then(function (res) {
+  //       setMaterials(res.data)
+  //     })
+  // }, [])
   
-  useEffect(() => {
-    axios.get('/recipes')
-      .then(function (res) {
-        setRecipes(res.data)
-      })
-  }, [isModalOpen])
+  // useEffect(() => {
+  //   axios.get('/recipes')
+  //     .then(function (res) {
+  //       setRecipes(res.data)
+  //     })
+  // }, [isModalOpen])
 
   function openModal() {
     setIsModalOpen(true)
@@ -33,10 +48,11 @@ const Recipes = () => {
   return (
     <Template>
       <div className='recipe-page'>
-        <div className='recipe-title'>
+        <div className='m-2 mb-0 col align-center'>
           <h1>Recipes</h1>
-          <h2 className='add-recipe' onClick={openModal}>Add Recipe</h2>
+          <h3 className='add-button mt-1' onClick={openModal}>Add Recipe</h3>
         </div>
+        <div className='line'>-</div>
         <RecipeCardContainer recipes={recipes} />
       </div>
 
@@ -44,7 +60,7 @@ const Recipes = () => {
         ariaHideApp={false}
         isOpen={isModalOpen}
       >
-        <AddRecipeModal materials={materials} />
+        {/* <AddRecipeModal materials={materials} /> */}
         <button className='close-modal' onClick={closeModal}>Close</button>
       </Modal>
     </Template>
