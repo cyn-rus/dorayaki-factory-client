ac7d5708cb09ac0e65d4bcdf02556236f0c8429c
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-23 13:13:22 +0700

    refactor add recipe modal

--- src/components/AddRecipeModal/index.tsx
+++ src/components/AddRecipeModal/index.tsx
@@ -1,11 +1,12 @@
+import { useMemo } from 'react'
 import { Formik, Field, Form, FormikProps, FormikValues } from 'formik'
 import { object, string, number } from 'yup'
 import type { RecipeType, MaterialType } from '../../types'
 import { axios } from '../../api'
-import { useMemo } from 'react'
 
 interface Props {
   materials: MaterialType[]
+  closeModal: Function
 }
 
 const initialValues = {
@@ -18,10 +19,15 @@ const validationSchema = object().shape({
   stock: number().integer().min(0)
 })
 
-const AddRecipeModal = ({materials} : Props) => {
-  const totalMaterial = useMemo(() => {
-    const obj = new Object()
-    // materials.map(())
+const AddRecipeModal = ({materials, closeModal} : Props) => {
+  const materialsName = useMemo(() => {
+    const tes = materials.map((material) => {
+      const obj: any = new Object()
+      obj.nama = material.nama_bahan
+      obj.stok = 0
+      // return material.nama_bahan
+    })
+
   }, [])
 
   async function submitAdd({nama_bahan, jumlah}: RecipeType) {
@@ -36,7 +42,7 @@ const AddRecipeModal = ({materials} : Props) => {
   }
 
   return (
-    <div className='add-modal-recipe'>
+    <div className='add-modal'>
       <h1 className='modal-title'>Add Recipe</h1>
       <Formik
         initialValues={initialValues}
@@ -44,18 +50,28 @@ const AddRecipeModal = ({materials} : Props) => {
         validationSchema={validationSchema}
       >
         {({ errors }: FormikProps<FormikValues>) => (
-          <Form>
-            <label htmlFor="nama_bahan">Recipe Name</label>
-            <div className='error-message'>
-              <p>{errors.nama_bahan || '*'}</p>
-            </div>
-            <Field name='nama_bahan' placeholder='Recipe Name' />
+          <Form className='form-container'>
+            <div className='forms'>
+              <div className='form-field'>
+                <label htmlFor="nama_bahan">Recipe Name</label>
+                <div className='input-form'>
+                  <p className='error-message'>{errors.nama_bahan || '*'}</p>
+                  <Field name='nama_bahan' placeholder='Recipe Name' />
+                </div>
+              </div>
 
-            <label htmlFor="stock">Stock</label>
-            <div className='error-message'>
-              <p>{errors.stock && 'Stock Invalid' || '*'}</p>
+              <div className='form-field'>
+                <label htmlFor="stock">Stock</label>
+                <div className='input-form'>
+                  <p className='error-message'>{errors.stock && 'Stock Invalid' || '*'}</p>
+                  <Field name='stock' placeholder='Stock' />
+                </div>
+              </div>
+            </div>  
+            <div className='row'>
+              <button className='submit-button' type='submit'>Add</button>
+              <button className='submit-button ml-2' onClick={() => closeModal()}>Cancel</button>
             </div>
-            <Field name='stock' placeholder='Stock' />
           </Form>
         )}
       </Formik>
