7c90d9ce934ad6702fdd3f89102e012c279cab8f
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-25 01:50:38 +0700

    add headers for axios api

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,4 +1,7 @@
 import axios from "axios"
 import { BACKEND_URL } from "../const"
-const instance = axios.create({ baseURL: `${BACKEND_URL}`})
+const instance = axios.create({
+  baseURL: `${BACKEND_URL}`,
+  headers: {'Access-Control-Allow-Origin': '*'}
+})
 export default instance
