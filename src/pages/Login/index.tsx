145b87921860e17ce0184094c50cc807ee717819
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-25 01:51:43 +0700

    validate login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import { useHistory } from "react-router-dom"
 import { Formik, Field, Form, FormikProps, FormikValues } from "formik"
 import { object, string } from 'yup'
-import { axios } from "../../api"
+import axios from 'axios'
+import useContext from '../../context'
 import { LogFormType } from '../../types'
-import { isAuth } from '../../helper'
+import { IsAuth } from '../../helper'
 
 interface LoginType {
   username: string
@@ -21,8 +23,10 @@ const validationSchema = object().shape({
 })
 
 const Login = () => {
+  const [isLoginInvalid, setIsLoginInvalid] = useState(false)
+  const { setData } = useContext()
   const history = useHistory()
-  const auth = isAuth()
+  const auth = IsAuth()
   const forms: LogFormType[] = [{
     type: 'username',
     placeholder: 'Username',
@@ -33,18 +37,22 @@ const Login = () => {
 
   document.title = 'Login | Mahi mahi'
   
-  async function submitLogin({username, password}: LoginType) {
+  async function submitLogin({username, password}: LoginType, resetForm: Function) {
     try {
-      const res = await axios.post('/login', {
+      const res = await axios.post('http://localhost:8005/login', {
         username: username,
         password: password,
       })
-
-      console.log(res)
-
-      history.push('/dashboard')
+      
+      if (res.data.length !== 0) {
+        setData(res.data[0])
+        history.push('/recipes')
+      } else {
+        resetForm()
+        setIsLoginInvalid(true)
+      }
     } catch (err) {
-      console.log(err)
+        setIsLoginInvalid(true)
     }
   }
 
@@ -58,7 +66,7 @@ const Login = () => {
         <div className='mt-2 col align-center'>
           <Formik
             initialValues={initialValues}
-            onSubmit={(values) => submitLogin(values)}
+            onSubmit={(values, { resetForm }) => submitLogin(values, resetForm)}
             validationSchema={validationSchema}
           >
             {({ errors }: FormikProps<FormikValues>) => (
@@ -69,11 +77,22 @@ const Login = () => {
                       <label htmlFor={form.type}>{form.placeholder}</label>
                       <div className='input-form'>
                         <p className='error-message'>{errors[form.type] || '*'}</p>
-                        <Field name={form.type} type={form.type} placeholder={form.placeholder} />
+                        <Field 
+                          name={form.type}
+                          type={form.type}
+                          placeholder={form.placeholder}
+                          onBlur={() => setIsLoginInvalid(false)}
+                        />
                       </div>
                     </div>
                   )}
                 </div>
+                <p
+                  className='error-message'
+                  style={isLoginInvalid ? {opacity: '1'} : {opacity: '0'}}
+                >
+                  Login Invalid
+                </p>
                 <button className='submit-button mt-2' type='submit'>Log In</button>
               </Form>
             )}
