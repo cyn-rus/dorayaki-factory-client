a98accd053a0d3d7bebee501477d2c09669c00d9
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-26 03:07:16 +0700

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import { Recipes, Materials, Login, Register, Requests } from './pages'
+import { ManagedAppContext } from './context/stateManager'
 import './styles/index.scss'
 
 function App() {
-
   return (
-    <Router>
-      <Route exact path='/'>
-        <Redirect to='/login' />
-      </Route>
-      <Switch>
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/recipes' component={Recipes} />
-        <Route exact path='/materials' component={Materials} />
-        <Route exact path='/requests' component={Requests} />
-      </Switch>
-    </Router>
+    <ManagedAppContext>
+      <Router>
+        <Route exact path='/'>
+          <Redirect to='/login' />
+        </Route>
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/recipes' component={Recipes} />
+          <Route exact path='/materials' component={Materials} />
+          <Route exact path='/requests' component={Requests} />
+        </Switch>
+      </Router>
+    </ManagedAppContext>
   )
 }
 
