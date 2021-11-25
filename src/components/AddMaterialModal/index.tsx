cb95dd8cc7c8086321e4fc76fbc04e3e96af5a4e
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-26 03:07:53 +0700

    refactor add material

--- src/components/AddMaterialModal/index.tsx
+++ src/components/AddMaterialModal/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Formik, Field, Form, FormikProps, FormikValues } from 'formik'
 import { object, string, number } from 'yup'
 import type { MaterialType } from '../../types'
@@ -18,16 +19,18 @@ const validationSchema = object().shape({
 })
 
 const AddMaterialModal = ({closeModal}: Props) => {
+  const [isAddInvalid, setIsAddInvalild] = useState(false)
+
   async function submitAdd({nama_bahan, stok}: MaterialType) {
-    // try {
-    //   await axios.post('/material/add', {
-    //     nama_bahan: nama_bahan,
-    //     stok: stok,
-    //   })
-        closeModal()
-    // } catch (err) {
-    //   console.log(err)
-    // }
+    try {
+      await axios.post('/addBahanBaku', {
+        nama_bahan: nama_bahan,
+        stok: stok,
+      })
+      closeModal()
+    } catch (_) {
+      setIsAddInvalild(true)
+    }
   }
 
   return (
@@ -45,7 +48,7 @@ const AddMaterialModal = ({closeModal}: Props) => {
                 <label htmlFor="nama_bahan">Material Name</label>
                 <div className='input-form'>
                   <p className='error-message'>{errors.nama_bahan || '*'}</p>
-                  <Field name='nama_bahan' placeholder='Material Name' />
+                  <Field onBlur={() => setIsAddInvalild(false)} name='nama_bahan' placeholder='Material Name' />
                 </div>
               </div>
 
@@ -53,11 +56,16 @@ const AddMaterialModal = ({closeModal}: Props) => {
                 <label htmlFor="stok">Stock</label>
                 <div className='input-form'>
                   <p className='error-message'>{(errors.stok && 'Stock Invalid') || '*'}</p>
-                  <Field name='stok' placeholder='Stock' />
+                  <Field onBlur={() => setIsAddInvalild(false)} name='stok' placeholder='Stock' />
                 </div>
               </div>
             </div>
-
+            <p
+              className='error-message'
+              style={isAddInvalid ? {opacity: '1'} : {opacity: '0'}}
+            >
+              Error on editing material
+            </p>
             <div className='row'>
               <button className='submit-button' type='submit'>Add</button>
               <button className='submit-button ml-2' onClick={() => closeModal()}>Cancel</button>
