b9c703a9a7cc7aca89fed1a42742a6aa18ea6290
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-11-26 03:07:33 +0700

    add more types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,15 @@
-export interface RecipeType {
+export interface IDataBahan {
   nama_bahan: string
   jumlah: number
+}
+
+export interface INamaBahan {
+  nama_bahan: string
+}
+
+export interface RecipeType {
+  nama_resep: string
+  data_bahan: IDataBahan[]
 }
 
 export interface MaterialType {
@@ -11,4 +20,14 @@ export interface MaterialType {
 export interface LogFormType {
   type: string
   placeholder: string
+}
+
+export interface RequestType {
+  request_name: string
+  nama_dorayaki: string
+  jumlah: number
+  status: number | string
+  ip: string
+  endpoint: string
+  timestamp: string
 }
